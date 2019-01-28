26452bfec523c16023a1971e3b63ec6edeb0cd02
Kvaale <6592743+bkvaale@users.noreply.github.com>
2019-01-28 08:48:06 +0100

    First commit

---
+++ pca10040/s132/ses/Output/ble_app_uart_pca10040_s132 Release/Obj/utf.d
@@ -0,0 +1,6 @@
+Output/ble_app_uart_pca10040_s132\ Release/Obj/utf.o: \
+ C:\Nordic\SDKs\mesh\nRF5_SDK_15.2.0_9412b96\external\utf_converter\utf.c \
+ C:\Nordic\SDKs\mesh\nRF5_SDK_15.2.0_9412b96\external\utf_converter\utf.h \
+ C:/Program\ Files\ (x86)/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.40/include/stddef.h \
+ C:/Program\ Files\ (x86)/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.40/include/__crossworks.h \
+ C:/Program\ Files\ (x86)/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.40/include/stdint.h
